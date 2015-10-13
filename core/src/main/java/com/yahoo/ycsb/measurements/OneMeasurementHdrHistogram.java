9a90c0b1d9a4f66bf2fcdf938c0dd75a73f5ed2e
kruthar <kruthar@gmail.com>
2015-10-13 09:25:29 -0500

    changed percentile readouts to be 80% and 95%

--- core/src/main/java/com/yahoo/ycsb/measurements/OneMeasurementHdrHistogram.java
+++ core/src/main/java/com/yahoo/ycsb/measurements/OneMeasurementHdrHistogram.java
@@ -101,8 +101,10 @@ public class OneMeasurementHdrHistogram extends OneMeasurement {
     exporter.write(getName(), "AverageLatency(us)", totalHistogram.getMean());
     exporter.write(getName(), "MinLatency(us)", totalHistogram.getMinValue());
     exporter.write(getName(), "MaxLatency(us)", totalHistogram.getMaxValue());
-    exporter.write(getName(), "95thPercentileLatency(us)", totalHistogram.getValueAtPercentile(90));
-    exporter.write(getName(), "99thPercentileLatency(us)", totalHistogram.getValueAtPercentile(99));
+    //exporter.write(getName(), "95thPercentileLatency(us)", totalHistogram.getValueAtPercentile(90));
+    //exporter.write(getName(), "99thPercentileLatency(us)", totalHistogram.getValueAtPercentile(99));
+    exporter.write(getName(), "80thPercentileLatency(us)", totalHistogram.getValueAtPercentile(80));
+    exporter.write(getName(), "95thPercentileLatency(us)", totalHistogram.getValueAtPercentile(95));
     
     exportReturnCodes(exporter);
   }
